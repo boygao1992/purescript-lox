436aa1596ab13ea7357cbbb1edcf1fcefc140a19
boygao1992 <wg2313@columbia.edu>
2021-08-03 11:44:14 -0400

    print some results in console for testing

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,28 @@
 module Main where
 
 import Prelude
-
+import Data.Either (Either(..))
+import Data.String as Data.String
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Class.Console as Effect.Class.Console
+import Lox.Lexer as Lox.Lexer
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  Effect.Class.Console.log $ lex "6-3/1"
+  Effect.Class.Console.log $ lex " ( 6 - 3 ) / 1 "
+  Effect.Class.Console.log
+    $ lex
+        """
+{- test comment 1 -}
+add x y = x + y
+
+value = " test string "
+"""
+
+lex :: String -> String
+lex input =
+  "\n" <> show input <> ":\n\n"
+    <> case Lox.Lexer.lex input of
+        Left parseError -> show parseError
+        Right xs -> Data.String.joinWith "\n" <<< map show $ xs
